fdda81767867f3f9cff5f45de4b92ac69489c2f4
Tagliapietra96 <tagliatek96@gmail.com>
2022-12-04 12:44:51 +0100

    add products banner

--- src/components/ProductsBanner.vue
+++ src/components/ProductsBanner.vue
@@ -1,7 +1,10 @@
 <template>
     <section class="bg-1">
         <div class="container">
-            
+            <div v-for="element in articlesList" :style="`width: ${100 / articlesList.length}%`">
+                <img :src="`/img/${element.img}`" alt="">
+                <h5>{{ element.title.toUpperCase() }}</h5>
+            </div>
         </div>
     </section>
 </template>
@@ -10,14 +13,79 @@
 
 <script>
 export default {
-    
+    data() {
+        return {
+            articlesList: [
+                {
+                    img: 'buy-comics-digital-comics.png',
+                    title: 'digital comics',
+                },
+                {
+                    img: 'buy-comics-merchandise.png',
+                    title: 'dc merchandise',
+                },
+                {
+                    img: 'buy-comics-subscriptions.png',
+                    title: 'subscription',
+                },
+                {
+                    img: 'buy-comics-shop-locator.png',
+                    title: 'comic shop locator',
+                },
+                {
+                    img: 'buy-dc-power-visa.svg',
+                    title: 'dc power visa',
+                },
+            ],
+        }
+    },
 }
 </script>
 
 
 
 <style scoped lang="scss">
-    section{
-        height: 200px;
+@use '../styles/partials/variables.scss' as *;
+
+section {
+    .container {
+        padding: $s-5 * 2;
+        display: flex;
+
+        div {
+            display: flex;
+            align-items: center;
+            color: white;
+
+            img {
+                height: $s-5 * 2;
+            }
+
+            h5 {
+                padding-left: $s-3;
+            }
+
+            &:last-of-type {
+                img {
+                    height: $s-5 * 1.5;
+                }
+            }
+        }
     }
+}
+
+@media screen and (max-width: 1000px) {
+    section {
+        .container {
+
+            div {
+                justify-content: center;
+
+                h5 {
+                    display: none;
+                }
+            }
+        }
+    }
+}
 </style>
